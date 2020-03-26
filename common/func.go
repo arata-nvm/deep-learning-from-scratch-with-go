b300b30c4e9f8ca7076da9ace43189d5de5a2204
arata <arata04962@gmail.com>
2020-03-26 15:27:44 +0900

    refactor: split apply function

--- common/func.go
+++ common/func.go
@@ -5,39 +5,33 @@ import (
 	"math"
 )
 
+func apply(m mat.Matrix, f func(float64) float64) mat.Matrix {
+	r, c := m.Dims()
+	result := mat.NewDense(r, c, nil)
+	result.Apply(func(i, j int, v float64) float64 {
+		return f(v)
+	}, m)
+	return result
+}
+
 func Step(x mat.Matrix) mat.Matrix {
-	step := func(i, j int, v float64) float64 {
+	return apply(x, func(v float64) float64 {
 		if v > 0.0 {
 			return 1.0
 		} else {
 			return 0.0
 		}
-	}
-
-	r, c := x.Dims()
-	result := mat.NewDense(r, c, nil)
-	result.Apply(step, x)
-	return result
+	})
 }
 
 func Sigmoid(x mat.Matrix) mat.Matrix {
-	sigmoid := func(i, j int, v float64) float64 {
+	return apply(x, func(v float64) float64 {
 		return 1 / (1 + math.Exp(v))
-	}
-
-	r, c := x.Dims()
-	result := mat.NewDense(r, c, nil)
-	result.Apply(sigmoid, x)
-	return result
+	})
 }
 
 func Relu(x mat.Matrix) mat.Matrix {
-	step := func(i, j int, v float64) float64 {
+	return apply(x, func(v float64) float64 {
 		return math.Max(0, v)
-	}
-
-	r, c := x.Dims()
-	result := mat.NewDense(r, c, nil)
-	result.Apply(step, x)
-	return result
+	})
 }
